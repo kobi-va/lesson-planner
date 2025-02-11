edc65465fc8b6684a50beda5c0de4f9e18a15937
kobi-va <93861348+kobi-va@users.noreply.github.com>
2025-02-11 16:17:30 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/lesson-planner/", // Required for GitHub Pages deployment
+  base: "/lesson-planner/", // Change this to match your GitHub repo name
+  build: {
+    outDir: "dist",
+  },
 });
